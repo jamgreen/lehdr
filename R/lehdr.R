359dfbe124571ac3e5062a21b92028efd106f002
lmwang9527 <lmwang@gmail.com>
2017-10-24 16:42:07 -0700

    1. Refactored the main function to handle multiple states x years; 2. Refactored the main function for a more concise (probably more efficient) implementation; 3. Added tests for the new functionality; 4. Added tests for rac data; 5. Bumped version to 0.2.0

--- R/lehdr.R
+++ R/lehdr.R
@@ -1,21 +1,23 @@
 #' 
 #' Download and load LODES data into a data frame (tibble)
-#'@param state US state abbreviation in lower case
-#'@param  year year of the lodes data
-#'@param  lodes_type table type: "origin-destination", "residential association", 
-#'or "workplace association". OD files give a home and destination census block
-#'for workers. Residential association files give job totals of worker home census 
-#'blocks and workplace association files give job totals of worker job census blocks
-#'@param job_type Jobtype: "JT00" for all jobs, "JT01" for Primary Jobs, "JT02" for All Private Jobs,
-#' "JT03" for Private Primary Jobs, "JT04" for All Federal Jobs, "JT05" for Federal Primary Jobs
-#'@param segment Segment of the workforce: "S000" total number of jobs for workers,
-#' "SA01" number of jobs forworker aged 29 or younger, "SA02" number of jobs for workers 
-#' aged 30-54,"SA03" number of jobs for workers 55 and older, "SE01" number of jobs 
-#' with earnings $1,250/month or less, "SE02" number of jobs with earnings $1,251 to 
-#' $3,333/month, "SE03" number of jobs with earnings greater than $3,333/month, 
-#' "SI01" number of jobs in Goods Producing industry sectors, "SI02" number of jobs in
-#' Trade, Transportation, and Utilities industry sectors, "SI03" number of jobs in All Other Services 
-#' industry sectors 
+#' 
+#' @param state US state abbreviation in lower case, can be a vector of states
+#' @param year year of the lodes data, can be a vector of years
+#' @param lodes_type table type: "origin-destination", "residential association", 
+#'        or "workplace association". OD files give a home and destination census block
+#'        for workers. Residential association files give job totals of worker home census 
+#'        blocks and workplace association files give job totals of worker job census blocks
+#' @param job_type Jobtype: "JT00" for all jobs, "JT01" for Primary Jobs, "JT02" for All Private Jobs,
+#'        "JT03" for Private Primary Jobs, "JT04" for All Federal Jobs, "JT05" for Federal Primary Jobs
+#' @param segment Segment of the workforce: "S000" total number of jobs for
+#'   workers, "SA01" number of jobs forworker aged 29 or younger, "SA02" number
+#'   of jobs for workers aged 30-54,"SA03" number of jobs for workers 55 and
+#'   older, "SE01" number of jobs with earnings $1,250/month or less, "SE02"
+#'   number of jobs with earnings $1,251 to $3,333/month, "SE03" number of jobs
+#'   with earnings greater than $3,333/month, "SI01" number of jobs in Goods
+#'   Producing industry sectors, "SI02" number of jobs in Trade, Transportation,
+#'   and Utilities industry sectors, "SI03" number of jobs in All Other Services
+#'   industry sectors
 #' @param state_part Part of the state file, can have values of "main" or "aux" in OD files. "Main"
 #' includes workers with their workplace and residence in the state. "Aux" includes workers with 
 #' residences out of state and workplace in the state of interest
@@ -27,35 +29,47 @@
 #' @import dplyr
 #' @importFrom readr read_csv cols col_character 
 #' @importFrom httr GET stop_for_status HEAD write_disk
-#' 
-#' 
+#' @importFrom glue glue
+#' @importFrom stats na.omit
+#'  
 #' @export
-#
-#
-#
-
 
 grab_lodes <- function(state, year, lodes_type = c("od", "rac", "wac"), 
                        job_type = c("JT00", "JT01", "JT02", "JT03", "JT04", "JT05"), 
                        segment = c("S000", "SA01", "SA02", "SA03", "SE01", "SE02",
                                    "SE03", "SI01", "SI02", "SI03"),  
-                       tract = FALSE, state_part = NULL, download_dir = getwd()) {
+                       tract = FALSE, state_part = NULL, 
+                       download_dir = file.path(getwd(), "lodes_raw")) {
+  
+  if (length(state) > 1 | length(year) > 1) {
+    ## Handle multiple states x years
+    state_year <- expand.grid(state=state, year=year)
+    results <- apply(state_year, 1, function(df_row) grab_lodes(df_row[1], df_row[2], 
+                                                           lodes_type = lodes_type,
+                                                           job_type = job_type,
+                                                           segment = segment,
+                                                           tract = tract,
+                                                           state_part = state_part,
+                                                           download_dir = download_dir) 
+                     )
+    return(dplyr::bind_rows(results))
+  }
   
   state <- tolower(state)
   type <- match.arg(tolower(lodes_type), c("od", "rac", "wac"))
   
-  url <- if(type == "od") {
-    url <- 
-      glue::glue("https://lehd.ces.census.gov/data/lodes/LODES7/{state}/{lodes_type}/{state}_{lodes_type}_{state_part}_{job_type}_{year}.csv.gz")
-  } else{
-    url <- 
-      glue::glue("https://lehd.ces.census.gov/data/lodes/LODES7/{state}/{lodes_type}/{state}_{lodes_type}_{segment}_{job_type}_{year}.csv.gz")
-  }
+  url <- ifelse(type == "od", 
+                glue::glue("https://lehd.ces.census.gov/data/lodes/LODES7/{state}/{lodes_type}/{state}_{lodes_type}_{state_part}_{job_type}_{year}.csv.gz"),
+                glue::glue("https://lehd.ces.census.gov/data/lodes/LODES7/{state}/{lodes_type}/{state}_{lodes_type}_{segment}_{job_type}_{year}.csv.gz")
+  )
   
   httr::stop_for_status(httr::HEAD(url),
                         "data for this combination of features was not found on LODES")
   
-  fil <- file.path(path.expand(download_dir), basename(url))
+  download_dir <- path.expand(download_dir)
+  if (!dir.exists(download_dir))
+    dir.create(download_dir)
+  fil <- file.path(download_dir, basename(url))
   
   if(file.exists(fil)) {
     message(glue::glue("Cached version of file found in {fil}\n Reading now..."))
@@ -64,44 +78,37 @@ grab_lodes <- function(state, year, lodes_type = c("od", "rac", "wac"),
     res <- httr::GET(url, httr::write_disk(fil))
   }
   
-  if (lodes_type != "od") {
+  col_types <- cols(w_geocode = col_character(), 
+                    h_geocode = col_character(),
+                    createdate = col_character())
+  
+  if (lodes_type == "rac")
+    col_types$cols$w_geocode <- NULL
+  if (lodes_type == "wac")
+    col_types$cols$h_geocode <- NULL
+  
+  df <- suppressMessages(readr::read_csv(fil, col_types = col_types))
+
+  df <- df %>% mutate(
+    year=year,
+    State=toupper(state))
+  
+  if (tract==TRUE) {
+    ## Convert *_geocode column(s) to tract id
+    df <- df %>% 
+      mutate_at(vars(ends_with("_geocode")), funs(stringr::str_sub(., 1, 11))) %>% 
+      rename_at(vars(ends_with("_geocode")), 
+                funs(stringr::str_replace(., "geocode", "tract_id")))
     
-    df <- suppressMessages(readr::read_csv(fil, 
-                                         col_types = cols(w_geocode = col_character(), 
-                                                        createdate = col_character())))
-  } else {
-    
-    df <- suppressMessages(readr::read_csv(fil, 
-                                           col_types = cols(w_geocode = col_character(),
-                                                            h_geocode = col_character(),
-                                                            createdate = col_character())))
+    ## Group by column(s) ending with "_tract_id"
+    tract_cols <- stringr::str_extract(names(df), ".*_tract_id$") %>% na.omit()
+    group_cols <- c("year", "State", tract_cols)
+    group_syms <- rlang::syms(group_cols)
+    df <- df %>% 
+      group_by(!!!group_syms) %>% 
+      summarise_if(is.numeric, funs(sum))
   }
-  
-  df <- df %>% 
-    mutate(year=year,
-           state=toupper(state))
-  
-  
-  if (tract == TRUE & lodes_type == "od") {
-    
-    df$w_tract_id <-  stringr::str_sub(df$w_geocode, 1, 11)
-    df$h_tract_id <- stringr::str_sub(df$h_geocode, 1, 11)
-    df <- df %>% select(-w_geocode, -h_geocode, -createdate)
-    df <- df %>% 
-      group_by(w_tract_id, h_tract_id, year) %>% 
-      summarise_if(is.numeric, funs(sum)) %>% 
-      mutate(State = toupper(state))
-    
-  } else if(tract == TRUE) {
-    
-    df$w_tract_id <-  stringr::str_sub(df$w_geocode, 1, 11)
-    df <- df %>% select(-w_geocode, -createdate)
-    df <- df %>% 
-      group_by(w_tract_id, year) %>% 
-      summarise_if(is.numeric, funs(sum))  %>% 
-      mutate(State = toupper(state))
-    }
-  
+
   return(df)
   
 }
--- tests/test_lehdr.R
+++ tests/test_lehdr.R
@@ -14,6 +14,15 @@ test_that("test grab lodes od", {
                  dim, c(1410831, 15))
 })
 
+test_that("test grab lodes rac", {
+  expect_equal(grab_lodes(state = 'or', year = 2014, lodes_type = "rac", job_type = "JT01", 
+                          segment = "SA01", tract = TRUE) %>% 
+                 dim, c(834, 44))
+  expect_equal(grab_lodes(state = "or", year = "2015", lodes_type = "rac", job_type = "JT01", 
+                          segment = "SA01") %>% 
+                 dim, c(60226, 45))
+})
+
 test_that("test grab lodes wac", {
   expect_equal(grab_lodes(state = 'or', year = 2014, lodes_type = "wac", job_type = "JT01", 
                           segment = "SA01", tract = TRUE) %>% 
@@ -21,4 +30,22 @@ test_that("test grab lodes wac", {
   expect_equal(grab_lodes(state = "or", year = "2015", lodes_type = "wac", job_type = "JT01", 
                           segment = "SA01") %>% 
                  dim, c(23367, 55))
+})
+
+test_that("test grab lodes od for multiple states and years", {
+  expect_equal(grab_lodes(state = c('or', "ri"), year = c(2013, 2014), lodes_type = "od", job_type = "JT01", 
+                          segment = "SA01", state_part = "main", tract = TRUE) %>% 
+                 dim, c(511071, 14))
+  expect_equal(grab_lodes(state = c('or', "ri"), year = c(2013, 2014), lodes_type = "od", job_type = "JT01", 
+                          segment = "SA01", state_part = "main" ) %>% 
+                 dim, c(3326655, 15))
+})
+
+test_that("test grab lodes wac for multiple states and years", {
+  expect_equal(grab_lodes(state = c('or', "ri"), year = c(2013, 2014), lodes_type = "wac", job_type = "JT01", 
+                          segment = "SA01", tract = TRUE) %>% 
+                 dim, c(2132, 54))
+  expect_equal(grab_lodes(state = c('or', "ri"), year = c(2013, 2014), lodes_type = "wac", job_type = "JT01", 
+                          segment = "SA01") %>% 
+                 dim, c(55307, 55))
 })
